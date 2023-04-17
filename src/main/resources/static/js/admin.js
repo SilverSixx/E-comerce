3823915b511797843915a4cfb657d98d55637a8f
SilverSixx <phamluongdat231103@gmail.com>
2023-04-17 15:25:36 +0700

    Initial version

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -2,33 +2,6 @@ $(document).ready(function(){
     $('#addUser').click(function(){
         $('#addUserModal').show();
     });
-
-    $('#addUserForm').submit(function(event){
-        // prevent default form submission
-        event.preventDefault();
-        let formData = {
-            firstName: $('#fname').val(),
-            lastName: $('#lname').val(),
-            password: $('#password').val(),
-            email: $('#email').val()
-        };
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:8090/api/v1/registration',
-            data: JSON.stringify(formData),
-            contentType: 'application/json',
-            success: function(){
-                showUsers()
-                alert('User added')
-                $('#addUserModal').hide();
-            },
-            error: function(jqXHR, textStatus){
-                alert('Error adding user: ' + textStatus);
-            }
-        });
-
-    });
-
     $('#showUsers').click(function(){
         showUsers();
     });
